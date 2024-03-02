372be16764eafd76f6b3d6774254bcc8d0d696ae
yambrosio <YENER.AMBROSIO@TOMZAGROUP.COM>
2024-03-02 07:56:39 -0600

    Commit initial

---
+++ app/src/main/java/com/yambrosio/todoapp/addtasks/ui/TasksScreen.kt
@@ -0,0 +1,8 @@
+package com.yambrosio.todoapp.addtasks.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TasksScreen(tasksViewModel: TasksViewModel) {
+
+}
---
+++ app/src/main/java/com/yambrosio/todoapp/addtasks/ui/TasksViewModel.kt
@@ -0,0 +1,7 @@
+package com.yambrosio.todoapp.addtasks.ui
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class TasksViewModel @Inject constructor() : ViewModel() {
+}
